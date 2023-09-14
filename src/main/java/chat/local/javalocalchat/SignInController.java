3c63e9b03ccb4e26442308d4495922731a0999cd
LoliKonch <ibugydydygig@gmail.com>
2023-09-14 22:42:39 +0300

    SignInController comments

--- src/main/java/chat/local/javalocalchat/SignInController.java
+++ src/main/java/chat/local/javalocalchat/SignInController.java
@@ -10,12 +10,23 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+/**
+ * Sign in window controller class
+ * @author Infobezdar'
+ * @version 1.0
+ */
 public class SignInController {
 
     private final ChangeWindow  ChangeWindow= new ChangeWindow();
 
+    /**
+     * Field regular expression for login
+     */
     private static final Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]");
 
+    /**
+     * Field regular expression for IP-address
+     */
     private static final Pattern IP_PATTERN = Pattern.compile(
             "^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
                   "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
@@ -44,21 +55,26 @@ public class SignInController {
     @FXML
     private Button signInButton;
 
+    /**
+     * Window initialization and control procedure
+     */
     @FXML
     void initialize() {
 
+        // Adding a limiter (40) to the password field
         TextFieldLimiter.addTextLimiter(passwordField, 40);
+        // Adding a limiter (40) to the login field
         TextFieldLimiter.addTextLimiter(loginField, 40);
 
-
+        // The event listener for the registration button opens a Sign_up window
         registrationButton.setOnAction(event ->
             ChangeWindow.changeWindowTo(sideBackground, "Sign_up.fxml"));
 
-
+        // The event listener for the forgot password button opens a Forgot_your_password window
         forgotPassword.setOnAction(event ->
             ChangeWindow.changeWindowTo(sideBackground, "Forgot_your_password.fxml"));
 
-
+        // The event listener for the enter button can open a Chat window
         signInButton.setOnAction(event ->{
 
             if (loginField.getText() != null && !loginField.getText().trim().isEmpty() &&
@@ -74,8 +90,9 @@ public class SignInController {
                         Client.setIPAddress(IPAddressField.getText().trim());
 
                         try {
+                            // Start connection to server
                             Client.startClient();
-
+                            // Send enter to chat request to server
                             Client.sendMessage(Client.getUsername());
                             Client.sendMessage(
                                     "sign_in" +
