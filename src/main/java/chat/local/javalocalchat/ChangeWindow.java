5027918639aecac7bed4b55e11fd1159936d0fa5
LoliKonch <ibugydydygig@gmail.com>
2023-09-14 21:30:44 +0300

    ChangeWindow comments

--- src/main/java/chat/local/javalocalchat/ChangeWindow.java
+++ src/main/java/chat/local/javalocalchat/ChangeWindow.java
@@ -10,22 +10,35 @@ import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
+/**
+ * Change window process controller class
+ * @author Infobezdar'
+ * @version 1.0
+ */
 public class ChangeWindow {
 
+    // Field will be removed soon
     public static String styleName = "dark_DS";
 
-    public void changeWindowTo(Pane background, String windowName, boolean sendBack) {
-        Stage lastStage = (Stage) background.getScene().getWindow();
+    /**
+     * Window remove and create procedure
+     * @param lastStageRoot - element of the last stage
+     * @param sendBack - flag to send to server
+     * @param windowName - name of new window
+     */
+    public void changeWindowTo(Pane lastStageRoot, String windowName, boolean sendBack) {
+        Stage lastStage = (Stage) lastStageRoot.getScene().getWindow();
         try {
-
+            // Loading new stage
             FXMLLoader loader = new FXMLLoader(getClass().getResource(windowName));
             loader.load();
-
+            // Customizing new stage
             Stage newStage = new Stage();
-            Parent root = loader.getRoot();
-            newStage.setScene(new Scene(root));
+            Parent newStageRoot = loader.getRoot();
+            newStage.setScene(new Scene(newStageRoot));
             newStage.setTitle("Secret Chat №1");
             newStage.setResizable(false);
+            // Send back to server
             newStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
                 public void handle(WindowEvent we) {
                     if (sendBack) {
@@ -34,7 +47,7 @@ public class ChangeWindow {
                     System.exit(0);
                 }
             });
-
+            // Applying styles to a scene
             Scene scene = newStage.getScene();
             String stylesheet = getClass().getResource("Registration_" + styleName + ".css").toExternalForm();
 
@@ -46,12 +59,18 @@ public class ChangeWindow {
             newStage.show();
 
             lastStage.close();
+
         } catch (IOException e) {
-            ExceptionBox.createExceptionBox(background, "    Can not find required system file");
+            ExceptionBox.createExceptionBox(lastStageRoot, "    Can not find required system file");
         }
     }
 
-    public void changeWindowTo(Pane background, String windowName) {
-        changeWindowTo(background, windowName, false);
+    /**
+     * Overloaded window remove and create procedure
+     * @param lastStageRoot - element of the last stage
+     * @param windowName - name of new window
+     */
+    public void changeWindowTo(Pane lastStageRoot, String windowName) {
+        changeWindowTo(lastStageRoot, windowName, false);
     }
 }
