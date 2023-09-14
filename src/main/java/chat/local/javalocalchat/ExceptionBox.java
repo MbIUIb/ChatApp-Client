fe532ed52e0e5596938b453007994a1c71fe5325
LoliKonch <ibugydydygig@gmail.com>
2023-09-14 21:43:43 +0300

    ExceptionBox comments

--- src/main/java/chat/local/javalocalchat/ExceptionBox.java
+++ src/main/java/chat/local/javalocalchat/ExceptionBox.java
@@ -5,21 +5,31 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
+/**
+ * Exception box controller class
+ * @author Infobezdar'
+ * @version 1.0
+ */
 public class ExceptionBox {
 
-    public static void createExceptionBox(Pane background, String exceptionText) {
-
+    /**
+     * Exception creation and animation procedure
+     * @param root - element of the stage
+     * @param exceptionText - text of the exception
+     */
+    public static void createExceptionBox(Pane root, String exceptionText) {
+        // Creation of exception
         Label exceptionLabel = new Label(exceptionText);
         exceptionLabel.getStyleClass().add("exception-label");
 
-        background.getChildren().add(exceptionLabel);
+        root.getChildren().add(exceptionLabel);
         exceptionLabel.setTranslateX(150);
         exceptionLabel.setTranslateY(400);
-
+        // Appearance of the exception
         TranslateTransition exLabelTranslationAppear = new TranslateTransition(Duration.millis(90), exceptionLabel);
         exLabelTranslationAppear.setByY(-100);
         exLabelTranslationAppear.play();
-
+        // Disappearance of the exception
         TranslateTransition exLabelTranslationDisappear = new TranslateTransition(Duration.millis(90), exceptionLabel);
         exLabelTranslationDisappear.setByY(100);
         exLabelTranslationDisappear.setDelay(Duration.millis(3000));
