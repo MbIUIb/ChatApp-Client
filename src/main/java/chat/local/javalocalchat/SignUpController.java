2b65a2286b5d840d6f3d04ea4dd3f328dec2c6c0
LoliKonch <ibugydydygig@gmail.com>
2023-09-14 22:55:35 +0300

    SignUpController comments

--- src/main/java/chat/local/javalocalchat/SignUpController.java
+++ src/main/java/chat/local/javalocalchat/SignUpController.java
@@ -9,10 +9,18 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+/**
+ * Sign up window controller class
+ * @author Infobezdar'
+ * @version 1.0
+ */
 public class SignUpController {
 
     private final ChangeWindow  ChangeWindow= new ChangeWindow();
 
+    /**
+     * Field regular expression for E-mail
+     */
     private static final Pattern RCF2822_MAIL_PATTERN = Pattern.compile(
             "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
                     "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
@@ -21,6 +29,9 @@ public class SignUpController {
                     "?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\" +
                     "\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
+    /**
+     * Field regular expression for login
+     */
     private static final Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]");
 
     @FXML
@@ -41,18 +52,24 @@ public class SignUpController {
     @FXML
     private Button signUpButton;
 
+    /**
+     * Window initialization and control procedure
+     */
     @FXML
     void initialize() {
 
+        // Adding a limiter (40) to the login field
         TextFieldLimiter.addTextLimiter(loginField, 40);
+        // Adding a limiter (40) to the password field
         TextFieldLimiter.addTextLimiter(passwordField, 40);
+        // Adding a limiter (100) to the E-mail field
         TextFieldLimiter.addTextLimiter(mailField, 100);
 
-
+        // The event listener for the back button opens a Sign_in window
         backButton.setOnAction(event ->
             ChangeWindow.changeWindowTo(sideBackground, "Sign_in.fxml"));
 
-
+        // The event listener for the creating an account request button can open an E-mail_confirmation window
         signUpButton.setOnAction(event ->{
             if (loginField.getText() != null && !loginField.getText().trim().isEmpty() &&
                 passwordField.getText() != null && !passwordField.getText().trim().isEmpty() &&
@@ -67,8 +84,9 @@ public class SignUpController {
                         Client.setEmail(mailField.getText().trim());
 
                         try {
+                            // Start connection to server
                             Client.startClient();
-
+                            // Send creating an account request to server
                             Client.sendMessage(Client.getUsername());
                             Client.sendMessage("sign_up" +
                                     "|" + Client.getUsername() +
