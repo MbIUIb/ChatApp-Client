e1568ef2784db7c443ef7348dd14621a5a611626
LoliKonch <ibugydydygig@gmail.com>
2023-09-14 22:12:29 +0300

    ForgotYourPasswordController comments

--- src/main/java/chat/local/javalocalchat/ForgotYourPasswordController.java
+++ src/main/java/chat/local/javalocalchat/ForgotYourPasswordController.java
@@ -8,10 +8,18 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+/**
+ * Request for changing password window controller class
+ * @author Infobezdar'
+ * @version 1.0
+ */
 public class ForgotYourPasswordController {
 
     private final ChangeWindow  ChangeWindow= new ChangeWindow();
 
+    /**
+     * Field regular expression for login
+     */
     private static final Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]");
 
     @FXML
@@ -26,16 +34,20 @@ public class ForgotYourPasswordController {
     @FXML
     private Pane sideBackground;
 
+    /**
+     * Window initialization and control procedure
+     */
     @FXML
     void initialize() {
 
+        // Adding a limiter (40) to the login field
         TextFieldLimiter.addTextLimiter(loginField, 40);
 
-
+        // The event listener for the back button opens a Sign_in window
         backButton.setOnAction(event ->
             ChangeWindow.changeWindowTo(sideBackground, "Sign_in.fxml"));
 
-
+        // The event listener for the change password request can open a New_password window
         confirmLoginButton.setOnAction(event ->{
             if (loginField.getText() != null && !loginField.getText().trim().isEmpty()) {
 
@@ -44,9 +56,9 @@ public class ForgotYourPasswordController {
                     Client.setUsername(loginField.getText().trim());
 
                     try {
-
+                        // Start connection to server
                         Client.startClient();
-
+                        // Send password recovery request to server
                         Client.sendMessage(Client.getUsername());
                         Client.sendMessage("password_recovery" +
                                 "|" + Client.getUsername());
