826d313e77a0e946a9ca1f5ea4b4c022ca835188
LoliKonch <ibugydydygig@gmail.com>
2023-09-14 22:27:13 +0300

    NewPasswordController comments

--- src/main/java/chat/local/javalocalchat/NewPasswordController.java
+++ src/main/java/chat/local/javalocalchat/NewPasswordController.java
@@ -5,6 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.layout.Pane;
 
+/**
+ * Set new password window controller class
+ * @author Infobezdar'
+ * @version 1.0
+ */
 public class NewPasswordController {
 
     private final ChangeWindow  ChangeWindow= new ChangeWindow();
@@ -19,7 +24,7 @@ public class NewPasswordController {
     private PasswordField newPasswordField;
 
     @FXML
-    private PasswordField secretCodeField;
+    private PasswordField confirmationCodeField;
 
     @FXML
     private Button setNewPasswordButton;
@@ -27,32 +32,38 @@ public class NewPasswordController {
     @FXML
     private Pane sideBackground;
 
+    /**
+     * Window initialization and control procedure
+     */
     @FXML
     void initialize() {
 
-        TextFieldLimiter.addTextLimiter(secretCodeField, 20);
+        // Adding a limiter (20) to the confirmation code field
+        TextFieldLimiter.addTextLimiter(confirmationCodeField, 20);
+        // Adding a limiter (40) to the password field
         TextFieldLimiter.addTextLimiter(newPasswordField, 40);
+        // Adding a limiter (40) to the confirmation password field
         TextFieldLimiter.addTextLimiter(confirmNewPasswordField, 40);
 
-
+        // The event listener for the back button opens a Sign_in window
         backButton.setOnAction(event -> {
             ChangeWindow.changeWindowTo(sideBackground, "Sign_in.fxml");
             Client.sendMessage("back");
         });
 
-
+        // The event listener for the confirm new password button can open a Sign_in window
         setNewPasswordButton.setOnAction(event ->{
 
             if (newPasswordField.getText() != null && !newPasswordField.getText().trim().isEmpty() &&
                 confirmNewPasswordField.getText() != null && !confirmNewPasswordField.getText().trim().isEmpty() &&
-                secretCodeField.getText() != null && !secretCodeField.getText().trim().isEmpty()) {
+                confirmationCodeField.getText() != null && !confirmationCodeField.getText().trim().isEmpty()) {
 
                 if (newPasswordField.getText().equals(confirmNewPasswordField.getText())) {
-
+                    // Send set new password request to server
                     Client.sendMessage("new_password +" +
                             "|" + Client.getUsername() +
                             "|" + confirmNewPasswordField.getText().trim() +
-                            "|" + secretCodeField.getText().trim());
+                            "|" + confirmationCodeField.getText().trim());
 
                     String answer = Client.waitMessage();
                     if (answer.equals("successful_password_recovery")) {
